3a3986122a883d7a552a6f320cc9e0ab9c1f9fa7
Carlos A. Dendarys <carlos@thirdlove.com>
2017-12-14 18:03:57 -0300

    I added code in order to handle the build fail

--- script-utils/deploy.sh
+++ script-utils/deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 if [ "$TRAVIS_BRANCH" == "master" ]; then
   echo "Running integration tests"
-  yarn run test-integration
+  COMPILE_RESULT=0 ; yarn run test-integration || COMPILE_RESULT=$?; expr $COMPILE_RESULT != 0
+  [ $COMPILE_RESULT -ge 2 ] && echo makefile error
 else
   echo "Integration test skipped"
 fi
