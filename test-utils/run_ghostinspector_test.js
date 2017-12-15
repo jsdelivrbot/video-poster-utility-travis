61bb7e5701bc5227af456f5fa23b4315c0c76fbe
Carlos A. Dendarys <carlos@thirdlove.com>
2017-12-15 16:19:26 -0300

    I added code in order to handle the build fail

--- test-utils/run_ghostinspector_test.js
+++ test-utils/run_ghostinspector_test.js
@@ -1,7 +1,7 @@
 var request = require('request');
 
 //GhostInspector API
-let ghostinspectorURL = 'https://api.ghostinspector.com/v1/tests/5a319034e684912918944780/execute/?apiKey=d3d0cc19fe8d69244a284e7061523b89542ba5dc'
+let ghostinspectorURL = 'https://api.ghostinspector.com/v1/tests/5a319034e684912918944780/execute/?apiKey=d3d0cc19fe8d69244a284e7061523b89542ba5dc';
 
 request(ghostinspectorURL, function (error, response, body) {
     if (!error && response.statusCode == 200) {
